8499ed37d82cce44f9aefd121ab337345b64acb9
Kevin Butler <kb@honeypot.io>
2020-12-04 01:39:17 +0100

    Two: part two

--- 2020/src/bin/two.rs
+++ 2020/src/bin/two.rs
@@ -33,6 +33,7 @@ fn main() {
 
     input.read_to_string(&mut s).unwrap();
     println!("a: {}", solve_a(&s));
+    println!("b: {}", solve_b(&s));
 }
 
 fn solve_a(input: &str) -> usize {
@@ -49,9 +50,54 @@ fn solve_a(input: &str) -> usize {
     }).count()
 }
 
+/*
+--- Part Two ---
+
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+    1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+    1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+    2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+How many passwords are valid according to the new interpretation of the policies?
+*/
+
+fn solve_b(input: &str) -> usize {
+    input.lines().filter(|line| {
+        let mut bytes = &mut line.trim().bytes();
+        let min: usize = read!("{}-", bytes);
+        let max: usize = read!("{} ", bytes);
+        let allowed_char: char = read!("{}:", bytes);
+        let password: String = read!("{}", bytes);
+
+        let first = password.as_bytes().get(min - 1).filter(|&&c| c == allowed_char as u8);
+        let last = password.as_bytes().get(max - 1).filter(|&&c| c == allowed_char as u8);
+
+        match (first, last) {
+            (Some(_), Some(_)) => false,
+            (None, None) => false,
+            _ => true
+        }
+    }).count()
+}
+
 #[test]
 fn smoke_a() {
     assert_eq!(solve_a("1-3 a: abcde
                         1-3 b: cdefg
                         2-9 c: ccccccccc"), 2);
+}
+
+#[test]
+fn smoke_b() {
+    assert_eq!(solve_b("1-3 a: abcde
+                        1-3 b: cdefg
+                        2-9 c: ccccccccc
+                        2-5 c: xcxx"), 2);
 }
