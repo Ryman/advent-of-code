de2f2d5359d23db729eb075b1872a813428c291d
Kevin Butler <kb@honeypot.io>
2020-12-06 10:47:28 +0100

    Eight: part two

--- 2020/src/bin/eight.rs
+++ 2020/src/bin/eight.rs
@@ -58,13 +58,14 @@ fn main() {
 
     input.read_to_string(&mut s).unwrap();
     println!("a: {}", solve_a(&s));
+    println!("b: {}", solve_b(&s));
 }
 
 #[derive(Debug, Copy, Clone)]
 enum Instruction {
     Acc(isize),
     Jump(isize),
-    Noop,
+    Noop(isize),
 }
 
 impl FromStr for Instruction {
@@ -79,45 +80,117 @@ impl FromStr for Instruction {
         Ok(match &*op {
             "acc" => Self::Acc(count),
             "jmp" => Self::Jump(count),
-            "nop" => Self::Noop,
+            "nop" => Self::Noop(count),
             unexpected => return Err(format!("Unexpected opcode: {}\nOriginal: [{}]", unexpected, s))
         })
     }
 }
 
-fn solve_a(input: &str) -> isize {
+fn run_machine(instructions: &[Instruction]) -> (bool, isize) {
     use Instruction::*;
-
-    let instructions: Vec<_> = input.lines()
-        .map(|s| s.parse::<Instruction>().unwrap())
-        .collect();
 
     let mut visited = vec![false; instructions.len()];
     let mut index: isize = 0;
     let mut acc: isize = 0;
 
     loop {
-        // println!("Executing[{}] {:?}: {}", index, instructions.get(index as usize), acc);
+        println!("Executing[{}] {:?}: {}", index, instructions.get(index as usize), acc);
 
-        if visited[index as usize] {
-            return acc;
-        } else {
-            visited[index as usize] = true;
+        match visited.get_mut(index as usize) {
+            Some(true) => return (false, acc),
+            Some(ref mut seen) => **seen = true,
+            None => return (true, acc),
         }
 
-        match instructions.get(index as usize) {
-            Some(Noop) => {},
-            Some(Acc(change)) =>  acc += *change,
-            Some(Jump(offset)) => {
-                index += *offset;
+        match instructions[index as usize] {
+            Noop(_) => {},
+            Acc(change) =>  acc += change,
+            Jump(offset) => {
+                index += offset;
 
                 continue
             },
-            None => unreachable!("Index out of bounds"),
         }
 
         index += 1;
     }
+}
+
+fn solve_a(input: &str) -> isize {
+    let instructions: Vec<_> = input.lines()
+        .map(|s| s.parse::<Instruction>().unwrap())
+        .collect();
+
+    let (_, result) = run_machine(&instructions);
+    result
+}
+
+/*
+--- Part Two ---
+
+After some careful analysis, you believe that exactly one instruction is corrupted.
+
+Somewhere in the program, either a jmp is supposed to be a nop, or a nop is supposed to be a jmp. (No acc instructions were harmed in the corruption of this boot code.)
+
+The program is supposed to terminate by attempting to execute an instruction immediately after the last instruction in the file. By changing exactly one jmp or nop, you can repair the boot code and make it terminate correctly.
+
+For example, consider the same program from above:
+
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+
+If you change the first instruction from nop +0 to jmp +0, it would create a single-instruction infinite loop, never leaving that instruction. If you change almost any of the jmp instructions, the program will still eventually find another jmp instruction and loop forever.
+
+However, if you change the second-to-last instruction (from jmp -4 to nop -4), the program terminates! The instructions are visited in this order:
+
+nop +0  | 1
+acc +1  | 2
+jmp +4  | 3
+acc +3  |
+jmp -3  |
+acc -99 |
+acc +1  | 4
+nop -4  | 5
+acc +6  | 6
+
+After the last instruction (acc +6), the program terminates by attempting to run the instruction below the last instruction in the file. With this change, after the program terminates, the accumulator contains the value 8 (acc +1, acc +1, acc +6).
+
+Fix the program so that it terminates normally by changing exactly one jmp (to nop) or nop (to jmp). What is the value of the accumulator after the program terminates?
+*/
+
+fn solve_b(input: &str) -> isize {
+    use Instruction::*;
+
+    let mut instructions: Vec<_> = input.lines()
+        .map(|s| s.parse::<Instruction>().unwrap())
+        .collect();
+
+    for index in 0..instructions.len() {
+        match instructions[index as usize] {
+            Noop(offset) => {
+                instructions[index as usize] = Jump(offset);
+                let (terminated, result) = run_machine(&instructions);
+                if terminated { return result }
+                instructions[index as usize] = Noop(offset);
+            },
+            Jump(offset) => {
+                instructions[index as usize] = Noop(offset);
+                let (terminated, result) = run_machine(&instructions);
+                if terminated { return result }
+                instructions[index as usize] = Jump(offset);
+            }
+            Acc(_) => continue,
+        }
+    }
+
+    unreachable!()
 }
 
 #[test]
@@ -131,4 +204,17 @@ fn smoke_a() {
                         acc +1
                         jmp -4
                         acc +6"), 5);
+}
+
+#[test]
+fn smoke_b() {
+    assert_eq!(solve_b("nop +0
+                        acc +1
+                        jmp +4
+                        acc +3
+                        jmp -3
+                        acc -99
+                        acc +1
+                        jmp -4
+                        acc +6"), 8);
 }
